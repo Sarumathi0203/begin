715a42e64653160334c08321113782bb54a0fe39
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2018-09-18 15:35:35 +0530

    Create prgrm3.c

---
+++ prgrm3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c=a;
+clrscr();
+if(c!=a)
+{
+printf("invalid\n");
+}
+else if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("vowel\n");
+}
+else
+{
+printf("consonant\n");
+}
+getch();
+}
