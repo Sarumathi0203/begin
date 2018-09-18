9be571278d3442152224946e8a7ed7ff42c4aaa7
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2018-09-18 15:59:53 +0530

    Create prgrm5.c

---
+++ prgrm5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+if((a>b)&&(a>c))
+{
+printf("%d\n",a);
+}
+else if(b>c)
+{
+printf("%d\n",b);
+}
+else
+{
+printf("%d\n",c)
+}
+getch();
+}
